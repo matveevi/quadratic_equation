fd49c5b7705d3e25c65ee3c7892cc1d12e30b673
matveevi <90386931+matveevi@users.noreply.github.com>
2021-09-09 20:24:06 +0300

    Rework some problems

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -29,38 +29,15 @@ int main() {
         return 0;
     }
 
-    double discriminant = 0;
-    discriminant = second_par * second_par - 4 * first_par * third_par;
-    printf("discriminant: %.10lf\n", discriminant);
-
-    double sqrt_of_discriminant = 0.;
     double x1 = 0;
     double x2 = 0;
-    double x = 0;
 
-    switch(NumberOfSolutions(discriminant)) {
+    switch(Solve(first_par, second_par, third_par, &x1, &x2)) {
         case 2 :
-            sqrt_of_discriminant = sqrt(discriminant);
-            
-            x1 = (-second_par - sqrt_of_discriminant) / (2 * first_par);
-            x2 = (-second_par + sqrt_of_discriminant) / (2 * first_par);
-
-            if (x1 < EXP && x1 > -EXP) {
-                x1 = 0.;
-            }
-            if (x2 < EXP && x2 > -EXP) {
-                x2 = 0.;
-            }
-
             printf("your roots are equal :\n x1: %.10lf x2: %.10lf\n", x1, x2);
             break;
         case 1 :
-            x = (- second_par / (2 * first_par));
-            if (x < EXP && x > -EXP) {
-                x = 0.;
-            }
-
-            printf("your root is equal :\n x: %.10lf\n", x);
+            printf("your root is equal :\n x: %.10lf\n", x1);
             break;
         case 0 :
             printf("No roots\n");
